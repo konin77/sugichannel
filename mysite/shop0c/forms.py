1b7f211ab4866b3f738b78fde31451aea166511a
Yuki Koni <fffff@gmail.com>
2026-06-11 14:08:06 +0900

    make forms

---
+++ mysite/shop0c/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+
+class LoginForm(forms.form):
+
+    
+    #def __init__(self, *args, **kwargs):
+    #    super().__init__(*args, **kwargs)
+
+    id = forms.CharField(label='会員ID', max_length=128, widget=forms.TextInput(attrs={'class':'form-control'}))
+    password = forms.CharField(label='パスワード', max_length=256, widget=forms.TextInput(attrs={'class':'form-control'}))
+
+class RegistUserForm(forms.form):
+
+    id = forms.CharField(label='会員ID', max_length=128, widget=forms.TextInput(attrs={'class':'form-control'}))
+    password = forms.CharField(label='パスワード', max_length=256, widget=forms.TextInput(attrs={'class':'form-control'}))
+    password_confirm = forms.CharField(label='パスワード', max_length=256, widget=forms.TextInput(attrs={'class':'form-control'}))
+    name = forms.CharField(label='お名前', max_length=128, widget=forms.TextInput(attrs={'class':'form-control'}))
+    address = forms.CharField(label='ご住所', max_length=256, widget=forms.TextInput(attrs={'class':'form-control'}))
+
+
+
