ea450ada9fea6b5634af86d14b8d242a9c044b71
Yuki Koni <fffff@gmail.com>
2026-06-12 09:01:48 +0900

    modify form

--- mysite/shop0c/forms.py
+++ mysite/shop0c/forms.py
@@ -6,16 +6,16 @@ class LoginForm(forms.Form):
     #def __init__(self, *args, **kwargs):
     #    super().__init__(*args, **kwargs)
 
-    id = forms.CharField(label='会員ID', max_length=128, widget=forms.TextInput(attrs={'class':'form-control'}))
-    password = forms.CharField(label='パスワード', max_length=256, widget=forms.TextInput(attrs={'class':'form-control'}))
+    id = forms.CharField(label='会員ID：', max_length=128, widget=forms.TextInput(attrs={'class':'form-control'}))
+    password = forms.CharField(label='パスワード：', max_length=256, widget=forms.TextInput(attrs={'class':'form-control'}))
 
 class RegistUserForm(forms.Form):
 
-    id = forms.CharField(label='会員ID', max_length=128, widget=forms.TextInput(attrs={'class':'form-control'}))
-    password = forms.CharField(label='パスワード', max_length=256, widget=forms.TextInput(attrs={'class':'form-control'}))
-    password_confirm = forms.CharField(label='パスワード', max_length=256, widget=forms.TextInput(attrs={'class':'form-control'}))
-    name = forms.CharField(label='お名前', max_length=128, widget=forms.TextInput(attrs={'class':'form-control'}))
-    address = forms.CharField(label='ご住所', max_length=256, widget=forms.TextInput(attrs={'class':'form-control'}))
+    id = forms.CharField(label='会員ID：', max_length=128, widget=forms.TextInput(attrs={'class':'form-control'}))
+    password = forms.CharField(label='パスワード：', max_length=256, widget=forms.TextInput(attrs={'class':'form-control'}))
+    password_confirm = forms.CharField(label='パスワード（確認）：', max_length=256, widget=forms.TextInput(attrs={'class':'form-control'}))
+    name = forms.CharField(label='お名前：', max_length=128, widget=forms.TextInput(attrs={'class':'form-control'}))
+    address = forms.CharField(label='ご住所：', max_length=256, widget=forms.TextInput(attrs={'class':'form-control'}))
 
 
 
